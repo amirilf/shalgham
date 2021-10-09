cefa8bc2a0f3692b69be852cca5550f91747db55
amir <amirilf@protonmail.com>
2021-10-09 20:44:11 +0330

    add some bg for website and edit some setting in setting.py and make it ok

--- src/site-project/blog/contents.py
+++ src/site-project/blog/contents.py
@@ -1,15 +1,17 @@
 # Dynamic Website Data
 
 data_set = {
-    'home_title'       : 'Home',
+    'home_title'       : 'Shalgham',
 
     #it's about text in col-4
     'about'            : 'Hello <b>werthtgrfe</b> and weclome to our site we\'ll write about technology and python, hope enjoy it and they\'re helpful for u',
  
     #these are header title and description in home.html
     'header_h'         : 'SHALGHAM',
-    'header_p'         : 'Know more about technology and specially Python and Js',
+    'header_p'         : 'What does shalgham lack from Apple? We make it famous',
     
+    'navbar_h'         : 'Shalgham.org',
+
     #this is to limit the number of display tags in col-4:
     'col_4_tags_count' : 10,
     
